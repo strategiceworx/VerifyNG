f957f5dfcdad7b76f69999428210aed108f55af2
Adeolu Adeyemi <adeolu@sholaadeyemi.com>
2017-12-06 15:03:44 +0100

    First Commit to Repository

---
+++ VerifyNG.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VerifyNG.WebUI.MvcApplication" Language="C#" %>
---
+++ VerifyNG.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VerifyNG.WebUI.Startup))]
+namespace VerifyNG.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
